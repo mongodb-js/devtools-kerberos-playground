cca18ec6e3218add9e66a6a34ad2423124508467
mcasimir <maurizio.cas@gmail.com>
2021-05-31 19:21:44 +0200

    wip

--- scripts/download-shell.ps1
+++ scripts/download-shell.ps1
@@ -1,6 +1,8 @@
-# Source file location
-$source = 'https://downloads.mongodb.com/windows/mongodb-shell-windows-x86_64-enterprise-4.4.6.zip'
-$destination = '.\shell.zip'
-
-#Download the file
-Invoke-WebRequest -Uri $source -OutFile $destination
+$Url = 'https://downloads.mongodb.com/windows/mongodb-shell-windows-x86_64-enterprise-4.4.6.zip'
+$DownloadZipFile = '.\shell.zip'
+$ExtractPath = ".\shell"
+Invoke-WebRequest -Uri $Url -OutFile $DownloadZipFile
+$ExtractShell = New-Object -ComObject Shell.Application
+$ExtractFiles = $ExtractShell.Namespace($DownloadZipFile).Items()
+$ExtractShell.NameSpace($ExtractPath).CopyHere($ExtractFiles)
+Start-Process $ExtractPath
